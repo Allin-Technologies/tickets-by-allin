68dad3ce9c32ba639a08133660308913e0c6c1cd
Queen Umesi <queenumesi01@gmail.com>
2024-10-06 11:48:18 +0100

    run build

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import * as path from "node:path";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from "vite-plugin-svgr";
-import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
