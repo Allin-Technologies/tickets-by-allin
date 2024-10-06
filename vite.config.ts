85a3fd638ec09d509642e56c001604369770279f
Queen Umesi <queenumesi01@gmail.com>
2024-10-06 11:31:59 +0100

    enable svgs to be used as components

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
-import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 import * as path from "node:path";
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
+import svgr from "vite-plugin-svgr";
+import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
-  plugins: [react(), TanStackRouterVite(),],
+  plugins: [react(), svgr(), TanStackRouterVite()],
   resolve: {
     alias: { "@": path.resolve(__dirname, "./src") },
   }
